ad205e5c949038a299e06698979dc75223a06550
J. Patrick Lanigan <planigan@gmail.com>
2017-07-03 19:37:41 -0400

    Fix minimum spark velocity scale based on canvas size and fire shells when they exit top

--- js/Shell.js
+++ js/Shell.js
@@ -7,14 +7,14 @@ function Shell(p) {
       ? p.random(0.1, 0.2)
       : x > p.width * 0.66 ? p.random(-0.2, -0.1) : 0
   const yVelScale = p.height / 100 + 1
-  this.velocity = p.createVector(randomX, p.random(-yVelScale, -3.5))
+  this.velocity = p.createVector(randomX, p.random(-yVelScale, -yVelScale / 2))
   this.gravity = p.createVector(0, 0.05)
   this.maxY = p.height
   this.alpha = p.random(1)
 }
 
 Shell.prototype.remove = function() {
-  return this.velocity.y > -1.5
+  return this.velocity.y > -1.5 || this.position.y < -30
 }
 
 Shell.prototype.update = function() {
